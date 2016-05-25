a875e5d28802858504b50ab81806badc1b5ac15b
gmasouras <masourasgeorge@gmail.com>
2016-05-26 01:25:41 +0200

    Print report method remains to complete Analyzable module

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,29 @@
 module Analyzable
   # Your code goes here!
+  def self.count_by_brand(products)
+  	products_brands = products.map {|product| product.brand}
+  	return attr_times(products_brands)
+  end
+
+  def self.count_by_name(products)
+  	products_names = products.map {|product| product.name}
+  	return attr_times(products_names)
+  end
+
+  def self.average_price(products)
+  	products_prices = products.map {|product| product.price.to_f}
+  	return (products_prices.reduce(:+)/products_prices.length).round(2)
+  end
+
+  private
+
+  def self.attr_times(product_attrs)
+  	product_attrs_set = product_attrs.uniq
+  	attr_times = {}
+  	product_attrs_set.each {|attribute|
+  		attr_times[attribute] = product_attrs.count(attribute)
+  	}
+  	return attr_times
+  end
+
 end
--- test_app.rb
+++ test_app.rb
@@ -1,13 +1,21 @@
 require_relative 'lib/product'
 require_relative 'lib/find_by'
+require_relative 'lib/analyzable'
 
 @data_path = File.dirname(__FILE__) + "/data/data.csv"
 CSV.open(@data_path, "wb") do |csv|
   csv << ["id", "brand", "product", "price"]
 end
 Product.create(brand: "WhatToys", name: "Apple Plant", price: 5.00)
-Product.create(brand: "AhatToys", name: "Apple Plant", price: 7.00)
+Product.create(brand: "BhatToys", name: "Apple Plant", price: 7.00)
 Product.create(brand: "BhatToys", name: "Apple Plant", price: 8.00)
 Product.create(brand: "WalterToys", name: "Lucky Notes", price: 31.00)
 Product.create(brand: "WalterToys", name: "Bucky Notes", price: 32.00)
+Product.create(brand: "AverageBrand", name: "Nyan Cat", price: 15.32)
+Product.create(brand: "AverageBrand", name: "Nyan Cat", price: 18.23)
+Product.create(brand: "AverageBrand", name: "Nyan Cat", price: 11.30)
+ 
+
+puts Analyzable::average_price(Product.where(brand: "AverageBrand"))
+
 
